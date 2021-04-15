3b0e72d01ee50e94b09cd1806a0f86803f874715
pbishal <pbishal60@gmail.com>
2021-04-15 12:51:47 +0530

    created contact

--- AddressBookSystem/Address.cs
+++ AddressBookSystem/Address.cs
@@ -6,5 +6,24 @@ namespace AddressBookSystem
 {
     class Address
     {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string email;
+        public int zip;
+        public int phoneNumber;
+        public Address(string firstName, string lastName, string address, string city, string state, string email, int zip, int phoneNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+        }
     }
 }
