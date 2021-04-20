73633eada70c77e4b486d42df9e9ac5b20370679
pbishal <pbishal60@gmail.com>
2021-04-20 21:54:29 +0530

    Refactor added

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,24 +6,46 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******* Welcome To AddressBook *******");
-
-            Console.Write("Enter First Name:- ");  // Take input 
-            string firstName = Console.ReadLine(); //Store input in firstName variable
-            Console.Write("Enter Last Name:- ");
-            string lastName = Console.ReadLine();
-            Console.Write("Enter Address:- ");
-            string address = Console.ReadLine();
-            Console.Write("Enter City:- ");
-            string city = Console.ReadLine();
-            Console.Write("Enter State:- ");
-            string state = Console.ReadLine();
-            Console.Write("Enter Zip Code :- ");
-            string zip = Console.ReadLine();
-            Console.Write("Enter Phone Number:- ");
-            string pNumber = Console.ReadLine();
-            Console.Write("Enter Email:- ");
-            string Email = Console.ReadLine();
+            Console.WriteLine("**** Welcome To AddressBook ****");
+            int i = 0;
+            Edit edit = new Edit(); //Create object Edit class
+            while (i == 0)
+            {
+                Console.WriteLine("\n1. Add New Person      ");
+                Console.WriteLine("2. Display Records     ");
+                Console.WriteLine("3. Edit Records        ");
+                Console.WriteLine("4. Delete Records      ");
+                Console.WriteLine("5. Exit		        \n");
+                Console.Write("Enter Your Choice:- ");
+                //String opt = Console.ReadLine();
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        edit.AddRecord(); //call AddRecord Method
+                        break;
+                    case 2:
+                        edit.DisplayRecord();  //call DisplayRecord Method
+                        break;
+                    case 3:
+                        Console.Write("Enter First Name To Edit Records:- ");
+                        String firstName = Console.ReadLine();
+                        edit.EditRecord(firstName); //call Edit record method
+                        break;
+                    case 4:
+                        Console.Write("Enter First Name To Delete Records:- ");
+                        String Name = Console.ReadLine();
+                        edit.DeleteRecord(Name); //call Delete record method
+                        break;
+                    case 5:
+                        i = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Enter Valid Option");
+                        break;
+                }
+            }
         }
     }
 }
+
