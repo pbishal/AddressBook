07d581f25d6ebb4a0f91a718af6b63be44852c9e
pbishal <pbishal60@gmail.com>
2021-04-20 10:53:55 +0530

    Contacts Created

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,7 +6,24 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to addressbook system");
+            Console.WriteLine("******* Welcome To AddressBook *******");
+
+            Console.Write("Enter First Name:- ");  // Take input 
+            string firstName = Console.ReadLine(); //Store input in firstName variable
+            Console.Write("Enter Last Name:- ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter Address:- ");
+            string address = Console.ReadLine();
+            Console.Write("Enter City:- ");
+            string city = Console.ReadLine();
+            Console.Write("Enter State:- ");
+            string state = Console.ReadLine();
+            Console.Write("Enter Zip Code :- ");
+            string zip = Console.ReadLine();
+            Console.Write("Enter Phone Number:- ");
+            string pNumber = Console.ReadLine();
+            Console.Write("Enter Email:- ");
+            string Email = Console.ReadLine();
         }
     }
 }
