f09b2c9b68850672bccd3e4e1a1ffbe5cbf4e2d1
pbishal <pbishal60@gmail.com>
2021-05-18 13:03:01 +0530

    Deleting a person

---
+++ AddressBookSystem/IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    interface IPerson
+    {
+        void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+        void EditContact(string name);
+        void DeleteContact(string name);
+        // void ViewContact();
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,23 +7,53 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("******* Welcome To AddressBook *******");
+            AddressBook addressBook = new AddressBook(); //object of AddessBook class
 
-            Console.Write("Enter First Name:- ");  // Take input 
-            string firstName = Console.ReadLine(); //Store input in firstName variable
-            Console.Write("Enter Last Name:- ");
-            string lastName = Console.ReadLine();
-            Console.Write("Enter Address:- ");
-            string address = Console.ReadLine();
-            Console.Write("Enter City:- ");
-            string city = Console.ReadLine();
-            Console.Write("Enter State:- ");
-            string state = Console.ReadLine();
-            Console.Write("Enter Zip Code :- ");
-            string zip = Console.ReadLine();
-            Console.Write("Enter Phone Number:- ");
-            string pNumber = Console.ReadLine();
-            Console.Write("Enter Email:- ");
-            string Email = Console.ReadLine();
+            int Choice = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Enter First Name :");
+                string firstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name :");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Enter Address :");
+                string address = Console.ReadLine();
+                Console.WriteLine("Enter City :");
+                string city = Console.ReadLine();
+                Console.WriteLine("Enter State :");
+                string state = Console.ReadLine();
+                Console.WriteLine("Enter Email :");
+                string email = Console.ReadLine();
+                Console.WriteLine("Enter Zip :");
+                int zip = Convert.ToInt32(Console.ReadLine());//conver into int using ToInt32()
+                Console.WriteLine("Enter Phone Number :");
+                long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
+
+
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter First Name Of Contact To Edit :");
+                        string nameToEdit = Console.ReadLine();
+                        addressBook.EditContact(nameToEdit);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter First Name Of Contact To Delete :");
+                        string nameToDelete = Console.ReadLine();
+                        addressBook.DeleteContact(nameToDelete);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter First Name Of Contact To View :");
+                        string nameToView = Console.ReadLine();
+                        addressBook.ViewContact();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("It is Not Found Any Information.");
+                        break;
+                }
+            } while (Choice != 4);
         }
     }
 }
